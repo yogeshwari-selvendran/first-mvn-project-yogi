d3abf51d74cbd93d92fe0a06549dea73c2c7be0f
yogeshwari-selvendran <116252201+yogeshwari-selvendran@users.noreply.github.com>
2023-06-23 10:56:57 +0530

    Add files via upload

---
+++ src/test/java/com/fssa/yogeshwari/day17/practice/HelloTest.java
@@ -0,0 +1,23 @@
+package com.fssa.yogeshwari.day17.practice;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Sample test class
+ * 
+ * @author BharathwajSoundarara
+ *
+ */
+public class HelloTest {
+
+	/**
+	 * First test case
+	 */
+	@Test
+	public void helloTest() {
+
+		Assertions.assertEquals("Hello World", "Hello World");
+	}
+}
